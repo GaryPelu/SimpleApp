fbee6969f17eb5373c7f3296512622893df9a8ec
GaryPelu <garypel@frontier.com>
2016-12-12 11:44:46 -0800

    adding source code to SimpleApp

---
+++ SimpleApp/SimpleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            App app = new App();
+
+            return app.Run(args);
+        }
+    }
+
+    public class App
+    {
+        public int Run(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return 1;
+            }
+
+            string foo = Reverse(args[0]);
+            Console.WriteLine(foo);
+            return 0;
+        }
+
+        // yeah, I know there's a reverse method but for the sake of demonstrating Unit Testing...
+        public string Reverse(string s)
+        {
+            char[] r = new char[s.Length];
+
+            int j = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                r[j++] = s[i];
+            }
+
+            return new string(r);
+        }
+    }
+}
---
+++ SimpleApp/SimpleAppTests/AppTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Tests
+{
+    [TestClass()]
+    public class AppTests
+    {
+        [TestMethod]
+        public void AppArgumentsValidTest()
+        {
+            App app = new App();
+
+            int actual = app.Run(null);
+            int expected = 1;
+
+            Assert.AreEqual(expected, actual, "Run did not fail with null arguments");
+
+        }
+
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            App app = new App();
+            string s = "Hello";
+            string expected = "olleH";
+            string actual = app.Reverse(s);
+
+            int test = String.Compare(expected, actual);
+
+            Assert.AreEqual(0, test, "Reverse did not reverse the string.  Expected {0}. Actual {1}", expected, actual);
+
+        }
+    }
+}
